d7abc2b29b37c1f65b295dc4785a1f6b5895631f
Nick <npoltz@gmail.com>
2021-03-28 17:36:11 -0600

    Stub in sensor readings. Add logic to send message to API.

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,71 @@
+#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 
-#define LED LED_BUILTIN
+#define SLEEP_TIME_SECONDS 600 // 10 minutes
+#define ALLOWED_CONNECT_CYCLES 20
+#define ALLOWED_READ_CYCLES 40
 
-//SSID and Password of your WiFi router
-const char* ssid = "";
-const char* password = "";
+const String wifiSSID = "";
+const String wifiPassword = "";
 
-//==============================================================
-//                  SETUP
-//==============================================================
-void setup(void){
-  pinMode(LED, OUTPUT);
+const IPAddress hostIp(10, 0, 0, 250);
+const int hostPort = 5020;
+
+const char* uri = "http://10.0.0.250:5020/v1/datalog/";
+const char* sensorId = "s01";
+
+void setup() {
+
   Serial.begin(9600);
+  /* 
+   *  If after changing WiFi settings (SSID or password) ESP8266
+   *  does not want to connect again, uncomment this line and reflash
+   */
+  //ESP.eraseConfig();
   
-  WiFi.begin(ssid, password);     //Connect to your WiFi router
-
-  // Wait for connection
+  WiFi.begin(wifiSSID, wifiPassword);
+  
+  uint8_t counter = 0;
   while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(LED,HIGH);
-    delay(200);
-    Serial.print(".");
-    digitalWrite(LED,LOW);
-    delay(200);
+    delay(500);
+    counter++;
+    if (counter > ALLOWED_CONNECT_CYCLES) {
+      ESP.deepSleep(SLEEP_TIME_SECONDS * 1000000);
+    }
+  }
+ 
+  WiFiClient wifiClient;
+
+  if (wifiClient.connect(hostIp, hostPort)) {
+    digitalWrite(0, HIGH);
+
+    float humidity;
+    float temperature;
+    counter = 0;
+    do {
+      humidity = random(50, 70);
+      temperature = random(18, 22);
+      counter++;
+      if (counter > ALLOWED_READ_CYCLES) {
+        ESP.deepSleep(SLEEP_TIME_SECONDS * 1000000);
+      }
+    } while (temperature == 85.0 || temperature == (-127.0));
+
+    Serial.println("Humidity: " + String(humidity) + ", Temperature: " + String(temperature));
+
+    HTTPClient httpClient;
+    httpClient.begin(wifiClient, uri);
+    httpClient.addHeader("Content-Type", "application/json");
+    int httpResponseCode = httpClient.POST("{\"sensorId\":\"" + String(sensorId) + "\",\"temperature\":\"" + String(temperature) + "\",\"relativeHumidity\":\"" + String(humidity) + "\"}");
+
+    Serial.println("HTTP Response code: " + String(httpResponseCode));
+      
+    httpClient.end(); 
   }
 
-  //If connection successful show IP address in serial monitor
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+  delay(2000);
+  ESP.deepSleep(SLEEP_TIME_SECONDS * 1000000);
 }
 
-//==============================================================
-//                     LOOP
-//==============================================================
-void loop(void){
-  digitalWrite(LED, LOW);
-  delay(2000);
-  digitalWrite(LED, HIGH);
-  delay(500);
+void loop() {
 }
