dea6037653630a99dc6c1dcf3c9b338612af2c86
Nick <npoltz@gmail.com>
2021-05-02 13:33:35 -0600

    Add logic to take readings from DHT22 sensor.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <DHT.h>
 
-#define SLEEP_TIME_SECONDS 600 // 10 minutes
+#define SLEEP_TIME_SECONDS 900 // 15 minutes
 #define ALLOWED_CONNECT_CYCLES 20
 #define ALLOWED_READ_CYCLES 40
+#define DHT_PIN D4
+#define DHT_TYPE DHT22
 
 const String wifiSSID = "";
 const String wifiPassword = "";
@@ -14,15 +17,12 @@ const int hostPort = 5020;
 const char* uri = "http://10.0.0.250:5020/v1/datalog/";
 const char* sensorId = "s01";
 
+DHT dht(DHT_PIN, DHT_TYPE);
+
 void setup() {
 
   Serial.begin(9600);
-  /* 
-   *  If after changing WiFi settings (SSID or password) ESP8266
-   *  does not want to connect again, uncomment this line and reflash
-   */
-  //ESP.eraseConfig();
-  
+
   WiFi.begin(wifiSSID, wifiPassword);
   
   uint8_t counter = 0;
@@ -33,18 +33,17 @@ void setup() {
       ESP.deepSleep(SLEEP_TIME_SECONDS * 1000000);
     }
   }
- 
+  
   WiFiClient wifiClient;
 
   if (wifiClient.connect(hostIp, hostPort)) {
-    digitalWrite(0, HIGH);
-
+    dht.begin();
     float humidity;
     float temperature;
-    counter = 0;
+    
     do {
-      humidity = random(50, 70);
-      temperature = random(18, 22);
+      humidity = dht.readHumidity();
+      temperature = dht.readTemperature();
       counter++;
       if (counter > ALLOWED_READ_CYCLES) {
         ESP.deepSleep(SLEEP_TIME_SECONDS * 1000000);
@@ -56,7 +55,9 @@ void setup() {
     HTTPClient httpClient;
     httpClient.begin(wifiClient, uri);
     httpClient.addHeader("Content-Type", "application/json");
-    int httpResponseCode = httpClient.POST("{\"sensorId\":\"" + String(sensorId) + "\",\"temperature\":\"" + String(temperature) + "\",\"relativeHumidity\":\"" + String(humidity) + "\"}");
+    int httpResponseCode = httpClient.POST("{\"sensorId\":\"" + String(sensorId) + 
+                                            "\",\"temperature\":\"" + String(temperature) + 
+                                            "\",\"relativeHumidity\":\"" + String(humidity) + "\"}");
 
     Serial.println("HTTP Response code: " + String(httpResponseCode));
       
